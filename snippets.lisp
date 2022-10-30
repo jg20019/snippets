a8b8076ddedc9e1df55f423e237a2cd903952ace
John Gibson <jg20019@gmail.com>
2022-10-30 17:27:05 -0400

    feat: Load .env file

--- snippets.lisp
+++ snippets.lisp
@@ -2,6 +2,31 @@
 
 (in-package #:snippets)
 
+;;; ENV ;;;
+
+(defparameter *env* (make-hash-table :test #'equal))
+
+(defun load-env (line) 
+  "Save key=value in the environment."
+  (let* ((index (position #\= line))
+         (key (subseq line 0 index))
+         (value (subseq line (1+ index))))
+    (set-env key value)))
+
+(defun get-env (name) 
+  "Get value for uppercased NAME in env if it exists."
+  (gethash (string-upcase name) *env*))
+
+(defun set-env (name value)
+  "Set value for uppercased NAME in env to VALUE."
+  (setf (gethash (string-upcase name) *env*) value))
+
+#+nil
+(with-open-file (env-file (asdf:system-relative-pathname :snippets ".env"))
+  (loop for line =  (read-line env-file nil nil)
+        while line
+        do (load-env line)))
+
 ;;; ACCEPTOR ;;; 
 
 (defvar *acceptor* nil "hunchentoot acceptor")
@@ -27,3 +52,5 @@
 
 (hunchentoot:define-easy-handler (index :uri "/") () 
   (djula:render-template* +index.html+ nil))
+
+;;; DATABASE ;;;
