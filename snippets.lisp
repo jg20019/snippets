0c696edaaf710fce4d213dd7c2c796531743f0c3
John Gibson <jg20019@gmail.com>
2022-10-31 20:07:02 -0400

    feat: make working with env simpler
    Moved all of the code to load the environment into a function called
    load-env. The old load-env was put inside of that function to load the
    individual lines.
    
    list-env was added to see what the current environment variables were.

--- snippets.lisp
+++ snippets.lisp
@@ -6,26 +6,32 @@
 
 (defparameter *env* (make-hash-table :test #'equal))
 
-(defun load-env (line) 
-  "Save key=value in the environment."
-  (let* ((index (position #\= line))
-         (key (subseq line 0 index))
-         (value (subseq line (1+ index))))
-    (set-env key value)))
-
 (defun get-env (name) 
-  "Get value for uppercased NAME in env if it exists."
-  (gethash (string-upcase name) *env*))
+  "Get value for name in env."
+  (gethash name *env*))
 
 (defun set-env (name value)
-  "Set value for uppercased NAME in env to VALUE."
-  (setf (gethash (string-upcase name) *env*) value))
+  "Set value for name in env to value"
+  (setf (gethash name *env*) value))
 
-#+nil
-(with-open-file (env-file (asdf:system-relative-pathname :snippets ".env"))
-  (loop for line =  (read-line env-file nil nil)
+(defun load-env ()
+  "Load .env file"
+  (labels ((load-env* (line)
+             "Save line of form key=value into the environment"
+             (let* ((index (position #\= line))
+                    (key (subseq line 0 index))
+                    (value (subseq line (1+ index))))
+               (set-env key value))))
+    (with-open-file (env-file (asdf:system-relative-pathname :snippets ".env"))
+      (loop for line =  (read-line env-file nil nil)
         while line
-        do (load-env line)))
+        do (load-env* line)))))
+
+(defun list-env () 
+  "Print key=value pairs that are in the *env*"
+  (loop for key being the hash-keys of *env* 
+        using (hash-value value)
+        do (format t "~a=~a~%" key value)))
 
 ;;; ACCEPTOR ;;; 
 
